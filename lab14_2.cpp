41f5820a3b3200b1b52b54a706743d6579f7f9c4
worapit <drm.wmwork@gmail.com>
2021-12-28 15:30:30 +0700

    complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,36 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool update[][M],int s,int x,int y){
+    for(int i = 0;i < N;i++){
+        for(int j = 0;j < M;j++){
+            if(sqrt(pow(i-x,2)+pow(j - y,2)) + 1 <= s){
+                update[i][j]=1;
+            } 
+        }
+    }
+}
+void showImage(const bool A[][M]){
+    for(int k = 0;k < M+2 ;k++){
+        cout << "-";
+    }
+    cout << "\n";
+    for(int i = 0;i < N;i++){
+        cout << "|";
+        for(int j = 0;j < M;j++){
+            if(A[i][j] == 0){
+                cout << " " ;
+            }else if(A[i][j] == 1){
+                cout << "*";
+            }
+        }
+        cout << "|\n";
+    }
+    for(int k = 0;k < M+2 ;k++){
+        cout << "-";
+    }
+    cout << "\n";
+}
+
+
