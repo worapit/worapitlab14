ffc3ad6e700607ddee5e3797b1c7f0e1dcbf7a5c
worapit <drm.wmwork@gmail.com>
2021-12-28 13:47:45 +0700

    complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,53 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double x[],int t,double y[]){
+    int i;
+    double sum,sum1,sum2,sum3,z;
+    //Arithmetic Mean
+    for(i = 0;i < t;i++){
+        sum += x[i];
+    }
+    y[0] = (double) sum/t;
+
+    //Standard Deviation
+    double sd,j;
+    for(i = 0;i < t;i++){
+        sum1 += pow(x[i],2);
+    }
+    j = sum1/t - pow(y[0],2);
+    sd = sqrt(j);     
+    y[1] = sd;
+
+    //Geometric Mean
+    sum2 = 1;
+    for(i = 0;i < t;i++){
+        sum2 *= x[i];
+    }
+    z = (double) 1/t;
+    y[2] = pow(sum2,z);
+
+    //Harmonic Mean
+    for(i = 0;i < t;i++){
+        sum3 += 1/x[i];
+    }
+    y[3] = (double) t/sum3;
+
+
+    //max
+    double max = x[0],min = x[0];
+       for(i = 0;i<t;i++){
+           if(x[i] > max){
+               max = x[i];
+           }
+           if(x[i] < min){
+               min = x[i];
+           }
+       }
+    y[4] = max;
+    y[5] = min;
+
+}
+
+
